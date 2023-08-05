c24710b0174ebde3723c5098c6c639894a6e7635
Azmal <azmalgazi8@gmail.com>
2023-08-05 07:53:29 +0600

    navbar created, added in routes

--- CodeMates_test_project/src/Routes/Routes.jsx
+++ CodeMates_test_project/src/Routes/Routes.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../components/Home";
+import Main from "../Layout/Main";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home></Home>
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        }
+      ]
     },
   ]);
 
