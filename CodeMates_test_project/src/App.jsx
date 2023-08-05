15a8eb176e0a77b3e02eecd180625d333d455903
samsulalamasif <samsulalamasif@gmail.com>
2023-08-05 07:19:42 +0600

    Test push from Asif

--- CodeMates_test_project/src/App.jsx
+++ CodeMates_test_project/src/App.jsx
@@ -2,22 +2,22 @@
 import './App.css'
 
 function App() {
- 
+
 
   return (
     <>
       <div>
-        
+
       </div>
-      <h1 className='text-5xl text-center'>Vite + React</h1>
+      <h1 className='text-5xl text-center'>Code Mates</h1>
       <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
-      
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+
     </>
   )
 }
