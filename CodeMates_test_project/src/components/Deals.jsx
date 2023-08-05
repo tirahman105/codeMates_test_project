0f7156aa1ad7c6e083c3e3a34b67386962802ff7
samsulalamasif <samsulalamasif@gmail.com>
2023-08-05 23:35:16 +0600

    meet the team added by asif

--- CodeMates_test_project/src/components/Deals.jsx
+++ CodeMates_test_project/src/components/Deals.jsx
@@ -1,11 +1,76 @@
-import React from 'react';
+// import React from 'react';
 
 const Deals = () => {
     return (
-        <div>
-            Deals
-            
+
+        <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://img.freepik.com/free-photo/mixed-fruits-with-apple-banana-orange-other_74190-938.jpg?size=626&ext=jpg&ga=GA1.1.2016789159.1682986543&semt=sph)' }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="">
+                    <h1 className=" text-5xl font-bold text-white">Meet the team section</h1>
+                    <h1 className=" text-sm font-semibold text-white mt-5 mb-8">
+                        Contrary to popular belief, is not simply  random text. It has roots in a
+                        <br />
+                        piece of classical Latin literature from 45 BC, making it over 2000 years old.</h1>
+                    <div className="flex flex-col lg:flex-row gap-20 ">
+                        <div className="flex flex-col justify-center items-center w-52 mx-auto">
+                            <img className="w-36 h-36 rounded-full mb-5" src="https://img.freepik.com/free-photo/close-up-young-successful-man-smiling-camera-standing-casual-outfit-against-blue-background_1258-66609.jpg?size=626&ext=jpg&ga=GA1.2.2016789159.1682986543&semt=sph" alt="" />
+                            <h1 className="text-2xl font-bold">Awlad Hossain</h1>
+                            <p className="text-sm font-semibold my-2">Fruits Expert</p>
+                            <div className="flex justify-center gap-6">
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold">f</h1>
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold pb-1">x</h1>
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold">in</h1>
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-center items-center w-52 mx-auto">
+                            <img className="w-36 h-36 rounded-full mb-5" src="https://img.freepik.com/free-photo/handsome-young-man-with-new-stylish-haircut_176420-19637.jpg?size=626&ext=jpg&ga=GA1.2.2016789159.1682986543&semt=sph" alt="" />
+                            <h1 className="text-2xl font-bold">Awlad Hossain</h1>
+                            <p className="text-sm font-semibold my-2">Fruits Expert</p>
+                            <div className="flex justify-center gap-6">
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold">f</h1>
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold pb-1">x</h1>
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold">in</h1>
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-center items-center w-52 mx-auto">
+                            <img className="w-36 h-36 rounded-full mb-5" src="https://img.freepik.com/free-photo/confident-attractive-caucasian-guy-beige-pullon-smiling-broadly-while-standing-against-gray_176420-44508.jpg?size=626&ext=jpg&ga=GA1.2.2016789159.1682986543&semt=sph" alt="" />
+                            <h1 className="text-2xl font-bold">Awlad Hossain</h1>
+                            <p className="text-sm font-semibold my-3">Fruits Expert</p>
+                            <div className="flex justify-center gap-6">
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold">f</h1>
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold pb-1">x</h1>
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold">in</h1>
+
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-center items-center w-52 mx-auto">
+                            <img className="w-36 h-36 rounded-full mb-5" src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.2.2016789159.1682986543&semt=sph" alt="" />
+                            <h1 className="text-2xl font-bold">Rokib Hossain</h1>
+                            <p className="text-sm font-semibold my-3">Fruits Expert</p>
+                            <div className="flex justify-center gap-6">
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold">f</h1>
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold pb-1">x</h1>
+                                <h1 className="mx-auto w-6 h-6 rounded-full bg-[#F85559]
+                                 text-lg flex items-center justify-center font-bold">in</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
