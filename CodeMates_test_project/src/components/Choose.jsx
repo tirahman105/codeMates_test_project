89130313deac2dc538a7ffccdaa59f9bfe425710
Azmal <azmalgazi8@gmail.com>
2023-08-06 06:21:28 +0600

    choose added by azmal

--- CodeMates_test_project/src/components/Choose.jsx
+++ CodeMates_test_project/src/components/Choose.jsx
@@ -1,9 +1,98 @@
-import React from 'react';
 
 const Choose = () => {
     return (
-        <div>
-            Choose 
+        <div className="mx-20">
+            <div className="text-center w-1/2 mx-auto">
+            <h2 className="text-3xl font-bold">Who Choose Use</h2>
+            <p className="mt-5">Contrary to popular belief, is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+            </div>
+
+            <div className='mt-10 grid grid-cols-1 md:grid-cols-3 gap-10 justify-center'>
+
+            <div>
+
+              <div className='flex gap-5'>
+              <div>
+              <p className='w-14 h-14 border border-white rounded-full'></p>
+              </div>
+              <div>
+              <h3 className='text-xl font-bold'>Dedicated Team</h3>
+              <hr className='w-20 mt-3' />
+              <p className='mt-3'>Professional employees are there for you to pick the most amazing and fresh fruits.</p>
+              </div>
+              </div>
+
+              <div className='flex gap-5 mt-10'>
+              <div>
+              <p className='w-14 h-14 border border-white rounded-full'></p>
+              </div>
+              <div>
+              <h3 className='text-xl font-bold'>Fresh is Best</h3>
+              <hr className='w-20 mt-3' />
+              <p className='mt-3'>Professional employees are there for you to pick the most amazing and fresh fruits.</p>
+              </div>
+              </div>
+              <div className='flex gap-5 mt-10'>
+              <div>
+              <p className='w-14 h-14 border border-white rounded-full'></p>
+              </div>
+              <div>
+              <h3 className='text-xl font-bold'>Gift Certificates</h3>
+              <hr className='w-20 mt-3' />
+              <p className='mt-3'>Professional employees are there for you to pick the most amazing and fresh fruits.</p>
+              </div>
+              </div>
+              </div>
+
+
+            <div>
+            <img className="h-full w-full rounded-md" src="https://img.freepik.com/premium-psd/fruits-berries-isolated_88281-5327.jpg?size=626&ext=jpg&uid=R91079514&ga=GA1.2.220873417.1673880723&semt=sph" alt="" />
+            </div>
+
+            <div>
+            
+            <div className='flex gap-5'>
+           
+            <div>
+            <h3 className='text-xl font-bold text-end'>Integrated Supplier</h3>
+            <div className='flex justify-end'>
+            <hr className='w-20 mt-3 ' /></div>
+            <p className='mt-3'>Professional employees are there for you to pick the most amazing and fresh fruits.</p>
+            </div>
+            <div>
+            <p className='w-14 h-14 border border-white rounded-full'></p>
+            </div>
+            </div>
+
+            <div className='flex gap-5 mt-10'>
+           
+            <div className=''>
+            <h3 className='text-xl font-bold  text-end'>Sustainable Practice</h3>
+            <div className='flex justify-end'>
+            <hr className='w-20 mt-3 ' /></div>
+            <p className='mt-3'>Professional employees are there for you to pick the most amazing and fresh fruits.</p>
+            </div>
+            <div>
+            <p className='w-14 h-14 border border-white rounded-full'></p>
+            </div>
+            </div>
+
+            <div className='flex gap-5 mt-10'>
+           
+            <div className=''>
+            <h3 className='text-xl font-bold text-end'>The Benefits Only</h3>
+            <div className='flex justify-end'>
+            <hr className='w-20 mt-3 ' /></div>
+            <p className='mt-3'>Professional employees are there for you to pick the most amazing and fresh fruits.</p>
+            </div>
+            <div>
+            <p className='w-14 h-14 border border-white rounded-full'></p>
+            </div>
+            </div>
+
+            
+            </div>
+            </div>
         </div>
     );
 };
